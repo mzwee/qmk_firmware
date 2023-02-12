c2b70478e1cf88398c75f2d607f65ae50e81fa8a
mzwee <mzwee95@gmail.com>
2023-02-12 11:02:50 -0800

    Enable space cadet shift

--- keyboards/splitkb/kyria/keymaps/mzwee/keymap.c
+++ keyboards/splitkb/kyria/keymaps/mzwee/keymap.c
@@ -47,7 +47,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|                              |------+------+------+------+------+--------|
  * |Ctrl/Esc|   A  |   S  |   D  |   F  |   G  |                              |   H  |   J  |   K  |   L  | ;  : |Ctrl/' "|
  * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
- * | LShift |   Z  |   X  |   C  |   V  |   B  | [ {  |  (   |  |   )  |  ] } |   N  |   M  | ,  < | . >  | /  ? | RShift |
+ * |LShift/(|   Z  |   X  |   C  |   V  |   B  | [ {  |  (   |  |   )  |  ] } |   N  |   M  | ,  < | . >  | /  ? |RShift/)|
  * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
  *                        |      | LGUI | LAlt/| Space| Nav  |  | Sym  | Space| AltGr| RGUI | Menu |
  *                        |      |      | Enter|      |      |  |      |      |      |      |      |
@@ -56,7 +56,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT(
      KC_TAB  , KC_Q ,  KC_W   ,  KC_E  ,   KC_R ,   KC_T ,                                        KC_Y,   KC_U ,  KC_I ,   KC_O ,  KC_P , KC_BSPC,
      CTL_ESC , KC_A ,  KC_S   ,  KC_D  ,   KC_F ,   KC_G ,                                        KC_H,   KC_J ,  KC_K ,   KC_L ,KC_SCLN,CTL_QUOT,
-     KC_LSFT , KC_Z ,  KC_X   ,  KC_C  ,   KC_V ,   KC_B , KC_LBRC, KC_LPRN, KC_RPRN, KC_RBRC,  KC_N  ,   KC_M ,KC_COMM, KC_DOT ,KC_SLSH, KC_RSFT,
+     SC_LSPO , KC_Z ,  KC_X   ,  KC_C  ,   KC_V ,   KC_B , KC_LBRC, KC_LPRN, KC_RPRN, KC_RBRC,  KC_N  ,   KC_M ,KC_COMM, KC_DOT ,KC_SLSH, SC_RSPC,
                                 _______, KC_LGUI, ALT_ENT, KC_SPC ,   NAV  ,   SYM  , KC_SPC , KC_RALT, KC_RGUI, KC_APP
     ),
 
